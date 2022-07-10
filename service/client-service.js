183d4cd0a024601622fc3dd4140e5b70673f56e0
russellsocola <russell999nahum@gmail.com>
2022-07-10 18:51:07 -0500

    incluyo la libreria uuid para manejar los id

--- service/client-service.js
+++ service/client-service.js
@@ -1,12 +1,24 @@
 //abrir http (metodo,url)
-//CRUD - Metodos HTTP
+//CRUD - Metodos HTTP =>
 //CREATE - POST
 //READ - GET
 //UPDATE - PUT/PATCH
 //DELETE - DELETE
+
 //fetch APi
 const listaClientes = () => fetch("http://localhost:3000/perfil").then( (respuesta) => respuesta.json());
 
+const crearCliente = (nombre, email)=> { 
+  return fetch("http://localhost:3000/perfil", {
+    method : "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({nombre,email, id: uuid.v4()})
+  });
+}
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
