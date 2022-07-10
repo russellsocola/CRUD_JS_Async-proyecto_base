cc147ab563d51bc22d76ac7d4a18cc8ac39e35e2
russellsocola <russell999nahum@gmail.com>
2022-07-10 16:34:18 -0500

    refactorizando codigo

--- service/client-service.js
+++ service/client-service.js
@@ -1,38 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-
-  const linea = document.createElement('tr');
-  const contenido = `
-            <td class="td" data-td>
-            ${nombre}
-            </td>
-            <td>
-            ${email}
-            </td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>
-          `;
-  linea.innerHTML = contenido;
-  return linea;
-};
-
-const table = document.querySelector('[data-table]');
 //abrir http (metodo,url)
 //CRUD - Metodos HTTP
 //CREATE - POST
@@ -42,9 +7,6 @@ const table = document.querySelector('[data-table]');
 //fetch APi
 const listaClientes = () => fetch("http://localhost:3000/perfil").then( (respuesta) => respuesta.json());
 
-listaClientes().then((data)=>{
-  data.forEach(perfil => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-    table.appendChild(nuevaLinea);
-  });
-}).catch((error)=> alert('Ocurrio un error'));
+export const clientServices = {
+  listaClientes,
+};
